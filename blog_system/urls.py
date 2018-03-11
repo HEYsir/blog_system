619067da2646770a81c73fabd8df1ef361be8b25
HEYsir <wafx1314@gmail.com>
2018-03-12 00:27:39 +0800

    增加app解析的路由及视图配置

--- blog_system/urls.py
+++ blog_system/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^noinf/', include('noinf.urls')),
 ]
+
