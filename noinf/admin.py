a8bd0692ac8f9ddaa1da462654d96c2f5008df44
HEYsir <wafx-1314@163.com>
2018-11-04 18:20:49 +0800

    自定义admin的model改变显示
    文章列表增加publicdate和clickCount显示，编辑页修改默认显示

--- noinf/admin.py
+++ noinf/admin.py
@@ -7,7 +7,33 @@ admin.site.register(User)
 admin.site.register(NavCategory)
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Article)
+
 admin.site.register(Comment)
 admin.site.register(Links)
 admin.site.register(Ad)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    # admin 选项控制变更列表所显示的列
+    list_display = ('title', 'desc', 'date_publish', 'click_count',)
+    # admin 选项控制变更列表可以链接的列
+    list_display_links = ('title', 'desc',)
+    # admin 选项控制变更列表可以直接编辑的列
+#   list_editable = ('click_count',)
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'desc', 'content')
+        }),
+        ('高级设置', {
+            # 'classes': ('collapse',),
+            'fields': ('is_recommend', 'category', 'tag')
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+
+# 使用自定义的ModelAdmin来注册
+admin.site.register(Article, ArticleAdmin)
