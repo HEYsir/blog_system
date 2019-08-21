388b8a950efd6319f95ad307540dbedc67a305bd
HEYsir <wafx-1314@163.com>
2019-08-21 21:56:45 +0800

    用户信息展示
    获取模型verbox_name，主题实现用户信息及相关信息显示效果配置

--- noinf/views.py
+++ noinf/views.py
@@ -1,11 +1,45 @@
 from django.shortcuts import render
 from .models import *
+from django.apps import apps
 
+
+# def getmodelfield(appname, modelname, exclude):
+#     """
+#     获取model的verbose_name和name的字段
+#     """
+#     filed = User._meta.fields
+#     print(filed)
+#     fielddic = {}
+#
+#     params = [f for f in filed if f.name not in exclude]
+#
+#     for i in params:
+#         fielddic[i.name] = i.verbose_name
+#
+#     print(fielddic)
+#     return fielddic
+
+
+exclude = ['username','email','is_staff','last_login','password','last_name','date_joined','is_active','is_superuser']
 
 # 全局的settings文件的配置
 def global_setting(request):
     top_nav = NavCategory.objects.all()
-    userInfo = User.objects.all()[0]
+    userInfo = User.objects.all()[0] #在没有创建用户的情况下会导致访问失败
+    # user_fileds = User._meta.get_fields()
+    user_fileds = User._meta._get_fields(reverse=False, include_parents=False)
+    # print(user_fileds)
+
+    # 由于User继承子抽象类AbstractUser,AbstractUser中的所有成员都直接成为被继承的本地成员，而不是父模型成员
+    # 所以只能这种方式过滤，https://docs.djangoproject.com/en/2.1/ref/models/meta/#retrieving-all-field-instances-of-a-model
+    # include_parents
+    params = [f for f in user_fileds if f.name not in exclude]
+    fielddic = {}
+    for i in params:
+        fielddic[i.name] = i.verbose_name
+
+    # print(fielddic)
+
     return locals()
 
 
