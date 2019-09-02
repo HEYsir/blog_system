0e7701452cb8010028d8164385c13a3d2aaea7b8
HEYsir <wafx-1314@163.com>
2019-09-03 01:28:06 +0800

    apache+mod_wsgi部署WSGI脚本完善

--- blog_system/wsgi.py
+++ blog_system/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from os.path import join,dirname,abspath
 
 from django.core.wsgi import get_wsgi_application
 
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_system.settings")
 
 application = get_wsgi_application()
