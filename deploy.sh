e6494cf5e21d1f200a05d414e51031a1c6dad1c5
noinf <wafx-1314@163.com>
2023-03-30 00:54:07 +0800

    增加进入虚拟环境执行部署命令

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
 echo $1
@@ -6,5 +6,10 @@ echo $2
 newSrcPath=$1
 orgSrcPath=$2
 cp -rf ${newSrcPath}/* ${orgSrcPath}
+
+source ${orgSrcPath}/pyenv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+
 # uwsgi --reload uwsgi.ini  # 在虚拟环境内，shell无法找到uwsgi命令
 echo c > ${orgSrcPath}/uwsgi.fifo   # 依赖uwsgi.ini里的配置，当前会在项目根目录下生成
