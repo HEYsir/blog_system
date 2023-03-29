d0eb818fdcb4ce65b1ee854774c44a667c0acfe5
noinf <wafx-1314@163.com>
2023-03-30 00:23:06 +0800

    修改uwsgi重启方式

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ echo $2
 newSrcPath=$1
 orgSrcPath=$2
 cp -rf ${newSrcPath}/* ${orgSrcPath}
-uwsgi --reload uwsgi.ini
+# uwsgi --reload uwsgi.ini  # 在虚拟环境内，shell无法找到uwsgi命令
+echo c > ${orgSrcPath}/uwsgi.fifo   # 依赖uwsgi.ini里的配置，当前会在项目根目录下生成
