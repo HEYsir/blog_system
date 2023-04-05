a66b022c9b86e9b97e7f5cd823599da363e52958
noinf <wafx-1314@163.com>
2023-04-06 00:53:10 +0800

    修改增加nginx日志切片管理

--- logrotat.sh
+++ logrotat.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
 set -x -e
-LOGNAME="blog_system"
+
 PRJDIR=$(cd "$(dirname "$0")";pwd)
-LOGDIR="/var/log/uwsgi"
+LOGDIR="/var/log"
 
-orgLog=$LOGDIR/$LOGNAME.log
-
+LOGNAME="blog_system"
 DATE=`date -d "yesterday" +"%Y-%m-%d"`
-dstLog=$LOGDIR/$LOGNAME-${DATE}.log
-mv $orgLog $dstLog
 
+# uwsgi日志处理
+uwsgiLogDir=$LOGDIR/uwsgi
+orgLog=$uwsgiLogDir/$LOGNAME.log
+dstLog=$uwsgiLogDir/$LOGNAME-${DATE}.log
+mv $orgLog $dstLog
 echo l > $PRJDIR/uwsgi.fifo
 # touchfile="/home/logs/.touchforlogrotat"
 # touch $touchfile
-
 # 删除30天前的*.log文件
-find $LOGDIR -mtime +30 -type f -name \*.log -exec rm -f {} \; 
+find $uwsgiLogDir -mtime +30 -type f -name \*.log -exec rm -f {} \; 
+
+# nginx日志处理
+LOGNAME="blog_system"
+ngxLogDir=$LOGDIR/nginx
+# access log
+orgLog=$ngxLogDir/$LOGNAME.access.log
+dstLog=$ngxLogDir/$LOGNAME.access-${DATE}.log
+mv $orgLog $dstLog
+# error log
+orgLog=$ngxLogDir/$LOGNAME.error.log
+dstLog=$ngxLogDir/$LOGNAME.error-${DATE}.log
+mv $orgLog $dstLog
+
+kill -USR1 `cat ${ngxLogDir}/nginx.pid`
+find $ngxLogDir -mtime +30 -type f -name \*.log -exec rm -f {}
